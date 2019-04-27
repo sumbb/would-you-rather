f3e5a97a0f8e98075a54f432c9155b75b4e0b8ef
Sumedha <suma@Sumedhas-MacBook-Pro.local>
2019-04-27 22:54:32 +0530

    feat: Added PollResult component
    The component will view the result of answered question to the logged in user. The will be directed to this page when the question is already answered.

---
+++ src/components/Option.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Option extends Component {
+    render() {
+        return (
+            <div>
+                Option
+            </div>
+        )
+    }
+}
+
+export default Option;
--- src/components/PollResult.js
+++ src/components/PollResult.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Option from './Option'
 
 class PollResult extends Component {
     render() {
+        const { qid, author_name, author_avatar } = this.props
         return (
-            <div>POLL RESULT</div>
+            <div className='center'>
+                <div className='display-inline'>
+                    <div className='poll-result'>
+                        <div className='poll-result-heading'>
+                            <strong>{`Added by ${author_name}`}</strong>
+                        </div>
+                        <div className='poll-result-info'>
+                            <img 
+                                src={author_avatar}
+                                alt={`Avatar of ${author_name}`}
+                                className='poll-result-avatar'    
+                            />
+                            <div className='poll-result-redirect'>
+                                <h3>Results</h3>
+                                <Option qid={qid} option={"optionOne"}/>
+                                <Option qid={qid} option={"optionTwo"}/> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
 
-export default PollResult
+function mapStateToProps({ users, questions }, { qid }) {
+    const question = questions[qid]
+    const user = users[question.author]
+
+    return {
+        qid,
+        author_name : user.name,
+        author_avatar :  user.avatarURL
+    }
+
+}
+
+export default connect(mapStateToProps)(PollResult)
