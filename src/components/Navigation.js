96dbb7cb8c4790acb35bb62c2071f1b5e83a4473
Sumedha <suma@Sumedhas-MacBook-Pro.local>
2019-04-29 01:20:51 +0530

    feat : Added coloring the current tab
    The current tab will be colored red in the Navigation bar.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,21 @@ import { connect } from 'react-redux'
 import { isUserLoggedIn } from '../utils/common';
 import { setLoggedUser } from '../actions/loggedUser';
 
+const paths = [
+    {
+        path : '/',
+        name : 'Home'
+    },
+    {
+        path : '/add',
+        name : 'New Question'
+    },
+    {
+        path : '/leaderboard',
+        name : 'Leader Board'
+    }
+]
+
 class Navigation extends Component {
   handleSubmit(event) {
       event.preventDefault()
@@ -11,25 +26,23 @@ class Navigation extends Component {
       this.props.history.push('/')
   }  
   render() {
-      const { avatar, name} = this.props
+      const { avatar, name, location } = this.props
       return (
         <nav className='nav center'>
             <ul>
-                <li>
-                    <NavLink to='/'  activeClassName='active'>
-                    Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' activeClassName='active'>
-                     New Question
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' activeClassName='active'>
-                     Leader Board
-                    </NavLink>
-                </li>
+                {
+                    paths.map(({ path, name}) => (
+                        <li key={path}>
+                            <NavLink
+                                to={path}
+                                activeClassName='active'
+                                className={location.pathname === path ? `selected-tab` : ``}
+                                >
+                                {name}
+                            </NavLink>
+                        </li>
+                    ))
+                }
                 
                 {name !== null ? (
                     <Fragment>
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleAddQuestion } from '../actions/questions';
 import { isUserLoggedIn } from '../utils/common'
-import { Redirect, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import Login from './Login';
 
 class NewQuestion extends Component {
